1a0e8bbe4b5e63c27cb698ea3dfd335749c790fc
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-28 11:55:32 -0300

    chore: add and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  // Diz qual o arquivo inicial da aplicação ↓
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  // Diz qual arquivo que vai ser gerado com webpack ↓
+  output: {
+    // Pasta onde o arquivo será gerado ↓
+    path: path.resolve(__dirname, 'dist'),
+    // Nome do arquivo que será gerado ↓
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Informa que o webpack pode ler esses 2 tipos de arquivo ↓
+    extensions: ['.js', '.jsx']
+  },
+  // Informa como a aplicação vai se comportar diante da importação de cada tipo de arquivo ↓
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+}
