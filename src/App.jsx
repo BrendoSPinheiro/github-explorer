e2ee852618006ec83fd399ea6e41668c9672e396
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-03-28 20:25:40 -0300

    feat: create repository list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-
-import './styles/global.css';
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <RepositoryList />
   );
 }
