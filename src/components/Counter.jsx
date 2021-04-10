3ad7e3fea5e5da7c7eb4957d8ad8f9cc4eb4faeb
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-04-10 19:43:01 -0300

    feat: add Counter component for learn about states

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+export const Counter = () => {
+
+  function increment() {
+    console.log('Incrementing');
+  }
+
+  return (
+    <div>
+      <h2>0</h2>
+      <button
+        type="button"
+        onClick={increment}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
